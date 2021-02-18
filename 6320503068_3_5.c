1bd221f1639ab0a4cd05d2c7309806182a64a3a9
Piya <piya.rat@ku.th>
2021-02-18 17:40:20 +0700

    7

--- 6320503068_3_5.c
+++ 6320503068_3_5.c
@@ -1,1 +1,32 @@
+#include<stdio.h>
 
+    void main(){
+
+        int n ,prime[3]={0} ,i=0 ,j ,p=2 ,check=0;
+        scanf("%d",&n);
+        int nold=n;
+        if(n>0 && n<10000)
+        {
+            while(n>1)
+            {
+                if(n%p==0)
+                {
+                    prime[i]=p;
+                    n/=p;
+                    check++;
+                    i++;
+                }
+                p++;
+                if(check==3)
+                {
+                    break;
+                }
+            }
+            if(prime[0]!=0 && prime[1]!=0 && prime[2]!=0)
+            {
+                printf("%d is a Lucky Number.",nold);
+            }
+
+        }
+
+    }
