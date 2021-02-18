21aed06114ab3bf00b4c0664aaf5af370c224fa7
Piya <piya.rat@ku.th>
2021-02-18 16:57:50 +0700

    6

--- 6320503068_3_4.c
+++ 6320503068_3_4.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
 
+    void main(){
+
+        unsigned long long int x;
+        scanf("%llu",&x);
+        int i=x;
+        if(x>=0 && x<=2000000000)
+        {
+            while(i>=2)
+            {
+                if(i%2!=0)
+                {
+                    printf("%d",i);
+                    break;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+        }
+    }
